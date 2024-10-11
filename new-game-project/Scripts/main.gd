07bee948bcf23922da32e88bbb1e019b388dd628
unaiLarra <unai.larranaga@opendeusto.es>
2024-10-11 16:06:12 +0300

    Initial commit

---
+++ new-game-project/Scripts/main.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_load_image_button_up() -> void:
+	print('Loading image')
