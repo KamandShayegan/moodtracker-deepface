31eeb73595283f152d34c7856ed7299837aa228e
unaiLarra <unai.larranaga@opendeusto.es>
2024-10-12 22:04:01 +0300

    Animated label setting

--- new-game-project/Scripts/main.gd
+++ new-game-project/Scripts/main.gd
@@ -21,29 +21,37 @@ extends Control
 var url = 'http://127.0.0.1:5000/'
 var api_functions = ['analyze','verify']
 
-var de1_target_value: float = 0
-var de2_target_value: float = 0
-var de3_target_value: float = 0
-var de4_target_value: float = 0
+func _ready() -> void:
+	age_value.text = '?'
+	gender_value.text = '?'
+	race_value.text = '?'
+	emotion_label_1.text = '?'
+	emotion_label_2.text = '?'
+	emotion_label_3.text = '?'
+	emotion_label_4.text = '?'
 
 # This function is called when each frame is drawn, useful for making anything move, in our case the progress bars
 func _physics_process(delta: float) -> void:
-	dominant_emotion_1.value = lerp(dominant_emotion_1.value, de1_target_value, delta * 2.2)
 	dominant_emotion_1.get("theme_override_styles/fill").bg_color = progress_bar_gradient.sample(dominant_emotion_1.value/100)
-	dominant_emotion_2.value = lerp(dominant_emotion_2.value, de2_target_value, delta * 2.2)
 	dominant_emotion_2.get("theme_override_styles/fill").bg_color = progress_bar_gradient.sample(dominant_emotion_2.value/100)
-	dominant_emotion_3.value = lerp(dominant_emotion_3.value, de3_target_value, delta * 2.2)
 	dominant_emotion_3.get("theme_override_styles/fill").bg_color = progress_bar_gradient.sample(dominant_emotion_3.value/100)
-	dominant_emotion_4.value = lerp(dominant_emotion_4.value, de4_target_value, delta * 2.2)
 	dominant_emotion_4.get("theme_override_styles/fill").bg_color = progress_bar_gradient.sample(dominant_emotion_4.value/100)
 
 
 # This function is connected to the button up event of the LoadImageButton
 func _on_load_image_button_up() -> void:
-	de1_target_value = 0
-	de2_target_value = 0
-	de3_target_value = 0
-	de4_target_value = 0
+	dominant_emotion_1.value = 0
+	dominant_emotion_2.value = 0
+	dominant_emotion_3.value = 0
+	dominant_emotion_4.value = 0
+	age_value.text = '?'
+	gender_value.text = '?'
+	race_value.text = '?'
+	emotion_label_1.text = '?'
+	emotion_label_2.text = '?'
+	emotion_label_3.text = '?'
+	emotion_label_4.text = '?'
+
 	%FileDialog.popup()
 
 # The button shows a file-system popup, when a file is selected, this function is called
@@ -78,11 +86,6 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 
-	# We set the values to the labels
-	age_value.text = str(response['results'][0]['age'])
-	gender_value.text = str(response['results'][0]['dominant_gender'])
-	race_value.text = str(response['results'][0]['dominant_race'])
-
 	# We sort the emotions by percentage and set them to their respective progress bars
 	var emotions: Dictionary = response['results'][0]['emotion']
 	var keys = emotions.keys()
@@ -91,12 +94,44 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 	var emotion_value_pairs: Array = []
 	for k: String in keys:
 		emotion_value_pairs.append([k,emotions[k]])
+
+	# We set the values to the labels
+	set_animate_labels(
+		str(response['results'][0]['age']),
+		str(response['results'][0]['dominant_gender']),
+		str(response['results'][0]['dominant_race']), 
+		emotion_value_pairs)
+
 	# Set the values to the progress bars and labels
-	de1_target_value = emotion_value_pairs[0][1]
+
+
+func set_animate_labels(age: String, gender: String, race: String, emotion_value_pairs: Array) -> void:
+	var tween = create_tween()
+
+	age_value.text = age
+	age_value.visible_ratio = 0
+	gender_value.text = gender
+	gender_value.visible_ratio = 0
+	race_value.text = race
+	race_value.visible_ratio = 0
 	emotion_label_1.text = emotion_value_pairs[0][0]
-	de2_target_value = emotion_value_pairs[1][1]
+	emotion_label_1.visible_ratio = 0
 	emotion_label_2.text = emotion_value_pairs[1][0]
-	de3_target_value = emotion_value_pairs[2][1]
+	emotion_label_2.visible_ratio = 0
 	emotion_label_3.text = emotion_value_pairs[2][0]
-	de4_target_value = emotion_value_pairs[3][1]
+	emotion_label_3.visible_ratio = 0
 	emotion_label_4.text = emotion_value_pairs[3][0]
+	emotion_label_4.visible_ratio = 0
+
+	tween.tween_property(age_value, 'visible_ratio', 1, 0.4)
+	tween.tween_property(gender_value, 'visible_ratio', 1, 0.5)
+	tween.tween_property(race_value, 'visible_ratio', 1, 0.5)
+	tween.tween_interval(0.4)
+	tween.parallel().tween_property(dominant_emotion_1, 'value', emotion_value_pairs[0][1], 1.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.parallel().tween_property(emotion_label_1, 'visible_ratio', 1, 0.7)
+	tween.parallel().tween_property(dominant_emotion_2, 'value', emotion_value_pairs[1][1], 1.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.parallel().tween_property(emotion_label_2, 'visible_ratio', 1, 0.7)
+	tween.parallel().tween_property(dominant_emotion_3, 'value', emotion_value_pairs[2][1], 1.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.parallel().tween_property(emotion_label_3, 'visible_ratio', 1, 0.7)
+	tween.parallel().tween_property(dominant_emotion_4, 'value', emotion_value_pairs[3][1], 1.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.parallel().tween_property(emotion_label_4, 'visible_ratio', 1, 0.7)
